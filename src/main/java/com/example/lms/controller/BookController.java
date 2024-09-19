cbc8d92c92deedee799cfc7040e2eb5bf008e89e
sadai2it <ksadasiva2003@gmail.com>
2024-09-19 11:29:07 +0530

    Checking commit

--- src/main/java/com/example/lms/controller/BookController.java
+++ src/main/java/com/example/lms/controller/BookController.java
@@ -64,7 +64,7 @@ public class BookController {
 	        Book book = bookService.updateBookByName(bookId, bookName);
 
 	        if (book == null) {
-	            return new ResponseEntity<>("The book which you are searching for is not there", HttpStatus.NOT_FOUND);
+	            return new ResponseEntity<>("The book which you are searching is currently not there", HttpStatus.NOT_FOUND);
 	        }
 
 	        return new ResponseEntity<>(book.getBookName(), HttpStatus.ACCEPTED);
